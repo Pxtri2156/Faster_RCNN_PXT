3b463d4c0affc71c2cbd9dc314c3cbddf1d7c439
Pxtri2156 <Phamxuantri1562000tp3@gmail.com>
2020-11-21 03:57:22 +0000

    update register_data.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 TRAFFIC_SIGN_ANNOT_TRAIN_JSON = "/content/train_1151.json"
 TRAFFIC_SIGN_ANNOT_VALID_JSON = "/content/test_1151.json"
+TRAFFIC_SIGN_ANNOT_TEST_JSON = "/content/test.json"
 PATH_TRAIN_IMG = "/content/za_traffic_2020/traffic_train/images"
 PATH_VALID_IMG = "/content/za_traffic_2020/traffic_train/images"
+PATH_TEST_IMG = '/content/za_traffic_2020/traffic_public_test/images'
 PATH_MODEL_ZOO = "COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"
 PATH_MODEL_FINAL = "/content/drive/My Drive/[11-2020] Zalo AI Challenge/TrafficSignDetection/Detectron2/src/Faster_RCNN/Model_Non_pretrained"
-PATH_TEST_IMG = '/content/za_traffic_2020/traffic_public_test/images'
 
 NAME_TRAIN = 'data_train'
 NAME_TEST = "data_test"
 NAME_VALID = 'data_valid'
-TIME = " "
